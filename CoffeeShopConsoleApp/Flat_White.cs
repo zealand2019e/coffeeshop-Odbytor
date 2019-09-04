c59b9b1b4db06eb0924df1cb5f14bd2a9686b17b
Bartosz <DonMarchewa@gmail.com>
2019-09-04 14:07:52 +0200

    task 9

---
+++ CoffeeShopConsoleApp/Flat_White.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeShopConsoleApp
+{
+    class Flat_White: Coffee
+    {
+        public override string Strength()
+        {
+            return "Mild";
+        }
+
+        public override int price()
+        {
+            return 30;
+        }
+    }
+}
